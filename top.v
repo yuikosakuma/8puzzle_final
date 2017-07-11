b3f3582219236f8c91b9c2b145fc2ce257d0e179
sakuma <sakuma@plant.westlab>
2017-07-11 17:42:27 +0900

    Revert "Revert "20170711""
    This reverts commit e7c60233f703b1bc2f9fc40caf1b8611885357de.

--- top.v
+++ top.v
@@ -4,30 +4,30 @@ module top(clk, rst_n);
 	input wire pc_we, reg_we, mem_we;
 
 	//pc&imem
-	input wire [7:0] pc_in, pc_out;
+	input wire [5:0] pc_in, pc_out;
 	input wire [15:0] op;
 
 	//decoder
 	input wire [4:0] dst;
 	input wire [4:0] src0, src1;
-	input wire [16:0] dec_data;
+	input wire [39:0] dec_data;
 
 	//alu
-	input wire [3:0] alu_op;
-	input wire [16:0] alu_out;
+	input wire [4:0] alu_op;
+	input wire [39:0] alu_out;
 
 	//zf
 	input wire zf, zf_out;
 
 	//selector
 	input wire sel1, sel2;
-	input wire [16:0] sel1_out, sel2_out;
+	input wire [39:0] sel1_out, sel2_out;
 
 	//register
-	input wire [16:0] reg_data0, reg_data1;
+	input wire [39:0] reg_data0, reg_data1;
 
 	//memory
-	input wire [16:0] mem_data;//7?
+	input wire [39:0] mem_data;//7?
 
 //sel
 sel s1(.in0(dec_data), .in1(reg_data0), .sel(sel1), .out(sel1_out));
@@ -46,7 +46,7 @@ memory mem0(.in(alu_out), .out(mem_data), .addr(reg_data1), .rst_n(rst_n), .clk(
 pc pc0(.pc_in(pc_in), .pc_out(pc_out), .rst_n(rst_n), .clk(clk), .we(pc_we));
 
 //imem
-imem1 imem1(.pc(pc_out), .op(op));
+imem imem(.pc(pc_out), .op(op));
 
 //zf
 zf zf0(.clk(clk), .rst_n(rst_n), .zf_in(zf), .zf_out(zf_out));
